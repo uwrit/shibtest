9142aa5d62ee5478a45608bbbacdb39afa861a91
Cliff Spital <cspital@uw.edu>
2019-05-23 12:10:38 -0700

    initial commit

---
+++ Controllers/ProtectedController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ncshib.Controllers
+{
+    [Route("api/protected")]
+    [Produces("application/json")]
+    [ApiController]
+    public class ProtectedController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<object> Get()
+        {
+            var map = new Dictionary<string, string>();
+            foreach (var header in Request.Headers)
+            {
+                map.Add(header.Key, header.Value);
+            }
+            return Ok(map);
+        }
+    }
+}
+
+
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# shibtest
+
+This is a quick and dirty application that can help during configuration of a Shibboleth SP running in front of a .NET Core app.
+
